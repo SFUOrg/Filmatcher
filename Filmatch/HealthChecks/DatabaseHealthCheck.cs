730a22ed07b1729644fe340eb166d454c08f544f
Erdemaizer <radnaevera@gmail.com>
2025-11-17 15:13:29 +0700

    add csproj

---
+++ Filmatch/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,38 @@
+using Filmatch.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Filmatch.HealthChecks;
+
+public class DatabaseHealthCheck : IHealthCheck
+{
+    private readonly string _connectionString;
+
+    public DatabaseHealthCheck(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString(nameof(AppDbContext)) ??
+                            throw new Exception("Connection string not found");
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await using var sqlConnection = new SqlConnection(_connectionString);
+
+            await sqlConnection.OpenAsync(cancellationToken);
+
+            await using var command = sqlConnection.CreateCommand();
+            command.CommandText = "SELECT 1";
+
+            await command.ExecuteScalarAsync(cancellationToken);
+
+            return HealthCheckResult.Healthy("database is healthy");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy(ex.Message);
+        }
+    }
+}
